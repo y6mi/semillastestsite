ad3f612cacda31dfa44a88cf4a03356f6d563ef1
yami <81629507+y6mi@users.noreply.github.com>
2025-07-26 03:08:20 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
